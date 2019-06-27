138d0fa95de4fc23f644fdcd8ec4a1949e70be22
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-06-27 14:26:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,7 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.reduce(0) {|num| !!num == false}
+  source_array.reduce(0) {|num| !num == false}
 end
 
 def reduce_to_any_true(source_array)
