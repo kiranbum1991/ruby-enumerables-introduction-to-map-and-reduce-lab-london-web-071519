ceffcd1842d5376124122cea20b0ec1bf35f4449
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-06-27 14:58:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map_to_negativize(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push[i](source_array * -1)
+    new.push(source_array[i] * -1)
     i+=1
   end
 end
