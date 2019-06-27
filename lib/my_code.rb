86133419d0def35de5aa093ea490dad4204aae4b
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-06-27 14:16:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,27 @@
-def map_to_negativize (source_array)
-  new array =[]
-  i = 0
-  while i < source_array.length
-      [i] * -1
-      i += 1
-      source_array.puts[i]
+def map_to_negativize(source_array)
+  source_array.map {|x| x * -1}
+end
+
+def map_to_no_change (source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  source_array.map {|x| x * 2}
+end
+
+def map_to_square(source_array)
+  source_array.map {|x| x ** 2}
+end
+
+def reduce_to_total(source_array, starting_point=0)
+  source_array.reduce(starting_point) {|sum, n| sum + n}
+end
+
+def reduce_to_all_true(source_array)
+  source_array.reduce(0) {|num| num = true}
+end
+
+def reduce_to_any_true(source_array)
+  source_array.reduce(0) {|num| num = true}
 end
