a70fb7b87a50cc2adf5f05ecc620bb506f68cf3d
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-06-27 14:39:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,6 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.reduce(0)
   i = 0
   while i < source_array.length do
     return false if !source_array[i]
